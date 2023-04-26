8ff3f07d481c92df31c0e1695c215264d8e60e7d
btissamm <btissam.mhanni1@gmail.com>
2023-04-26 16:26:12 -0700

    e

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,24 +4,23 @@
 
 /**
  * main - Entry point
- * This program prints whether a random number is positive or negative
+ * Description: prints whether a randomly generated number is positive or negative
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
         int n;
 
         srand(time(0));
         n = rand() - RAND_MAX / 2;
-
         printf("%d is ", n);
-
         if (n > 0)
-            printf("positive\n");
+                printf("positive\n");
         else if (n == 0)
-            printf("zero\n");
+                printf("zero\n");
         else
-            printf("negative\n");
+                printf("negative\n");
 
         return (0);
 }
