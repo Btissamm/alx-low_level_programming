38dc0fbbfdf100dd6e8758587c461dd597a06ddd
btissamm <btissam.mhanni1@gmail.com>
2023-04-27 16:25:48 -0700

    r

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,12 @@
  * Return: Always 0 (Success)
 
  */
-int main() {
+int main(void) {
 	char c;
-	for(c = 'a'; c <= 'z'; ++c) {
+	for (c = 'a'; c <= 'z'; ++c)
+	{
 		putchar(c);
-    }
+	}
     putchar('\n');
     return 0;
 }
