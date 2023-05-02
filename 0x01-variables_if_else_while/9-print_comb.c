b0496b2139cb54bc959252c80462c4c05df70e9f
btissamm <btissam.mhanni1@gmail.com>
2023-05-02 16:54:33 -0700

    h

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+char i;
+char j;
+for (i = 48; i <= 57; i++)
+{
+	for (j = 48; j <= 57; j++)
+		{
+			putchar(ij)
+		}
+}
+return (0);
+}
