3c8bd62628a2b19efc956dc44e6de1da6115b8dd
btissamm <btissam.mhanni1@gmail.com>
2023-05-02 15:06:44 -0700

    h

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,7 @@ char ch = 'a';
 for (ch = 'a'; ch <= 'z'; ch++) 
 	putchar (ch);
 ch = 'A';
-for (ch = 'A'; ch = 'Z'; ch++)
+for (ch = 'A'; ch <= 'Z'; ch++)
 	putchar (ch);
 putchar ('\n');
 return (0);
