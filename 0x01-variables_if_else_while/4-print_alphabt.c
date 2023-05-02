bd1cd6788fe80b88b6cd0e1024c0115a585f1c74
btissamm <btissam.mhanni1@gmail.com>
2023-05-02 15:15:09 -0700

    h

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (char ch ='a'; (ch < 'z') && (ch != 'q' && 'e'); ch++) 
+	putchar (ch);
+return (0);
+}
