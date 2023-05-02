8ce468ee1e6760fb993cc4205728706b1b0b3252
btissamm <btissam.mhanni1@gmail.com>
2023-05-02 16:34:33 -0700

    h

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-/**main -Entry point
+/**
+ * main - Entry point
  *
- * Return: always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 char ch;
 for (ch = 'z'; ch >= 'a' ; ch--)
 	putchar(ch);
-putchar(ch);
+putchar('\n');
 return (0);
 }
