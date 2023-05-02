d29e20c349f8a82412101e1e07d6ae5b622bc6e9
btissamm <btissam.mhanni1@gmail.com>
2023-05-02 16:46:37 -0700

    h

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,10 +7,10 @@
 int main(void)
 {
 char ch;
-for (ch = 48; ch < 58; ch++)
-        putchar(ch);
+for (ch = 48; ch <= 58; ch++)
+	putchar(ch);
 for (ch = 'a'; ch < 'f'; ch++)
-        putchar(ch);
+	putchar(ch);
 putchar('\n');
 return (0);
 }
