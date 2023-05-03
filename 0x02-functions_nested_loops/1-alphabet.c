cef5063ef1494e86d0fd2ee0378abadecd18991f
btissamm <btissam.mhanni1@gmail.com>
2023-05-03 15:49:34 -0700

    e

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,17 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * main - Entry
  *
  * Return: 0
  */
+void print_alphabet(void)
+{
+char ch;
+        for (ch = 'a'; ch <= 'z'; ch++)
+        {
+                putchar(ch);
+        }
+}
 int main(void)
 { 
 	print_alphabet();
