0922c6832744c7957e7323c074e1fab8a2d8241a
btissamm <btissam.mhanni1@gmail.com>
2023-05-05 13:28:43 -0700

    t

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _abs - return absolute value
- * @i is the integer
+ * @i: is the integer
  * Return: abs
  */
 int _abs(int i)
 {
-	if(i<=0)
+	if (i <= 0)
 		return (-i);
 	else
-		return(i);
+		return (i);
 }
