c4fef47a3121adc6235cf8db2c1689d53bcd7cdd
btissamm <btissam.mhanni1@gmail.com>
2023-05-13 07:05:19 -0700

    e

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,7 +8,7 @@ void print_most_numbers(void)
         char a;
 
         for (a = 48; a <= 57; a++)
-        {if (a != (2 && 4))
+        {if (a != 2 && a != 4)
                 _putchar(a);
         }
         _putchar('\n');
