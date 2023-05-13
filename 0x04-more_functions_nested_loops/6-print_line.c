44ea128d2ef6d6184064a3a9c017ee1439c4e8ef
btissamm <btissam.mhanni1@gmail.com>
2023-05-13 08:26:58 -0700

    e

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * Return: a line form
+ */
+void print_line(int n)
+{
+int i;
+
+for (i = 0; i <=n; i++)
+{
+	_putchar('_');
+}
+}
