b8aeee37cf391a3931662ca0a165c005c38a0734
btissamm <btissam.mhanni1@gmail.com>
2023-05-13 05:42:06 -0700

    e

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers -  prints the numbers, from 0 to 9, followed by a new line
+ * 
+ * Return: 0
+ */
+ void print_numbers(void)
+{int a;
+	from (a = 48; a <= 57; a++)
+		_putchar(a);
+	return (0);
+}
