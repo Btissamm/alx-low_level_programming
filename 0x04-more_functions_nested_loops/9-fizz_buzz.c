ba810782fb852ee2e6ed738c256347592b33443e
btissamm <btissam.mhanni1@gmail.com>
2023-05-13 09:09:13 -0700

    e

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * fizzbuzz - game
+ * Return: fizz buzz test
+ */
+void fizzbuzz(void)
+{int i;
+	for (i = 0; i <= 100; i++)
+	{ 	if (i % 3 != 0 && i % 5 != 0)
+			printf("%d",i);
+		else if (i % 3 == 0)
+			printf("Fizz",i);
+		else if (i % 5 == 0)
+			printf("Buzz",i);
+	}
+}
