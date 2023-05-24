795929ce810e90afb4f9ae3bcb9027777f864fcc
btissamm <btissam.mhanni1@gmail.com>
2023-05-24 15:36:59 -0700

    r

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts -prints a string
+ * @str: is the string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+}
