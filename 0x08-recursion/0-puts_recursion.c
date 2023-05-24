fc7474a5f4b6e94a04adbd20c651f941bf9e99d3
btissamm <btissam.mhanni1@gmail.com>
2023-05-24 16:34:23 -0700

    z

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,9 +6,10 @@
  */
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
-	{	_putchar(*s);
-		s++;
+	if (*s == '\0')
+	{	_putchar('\n');
 	}
-	_putchar('\n');
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
