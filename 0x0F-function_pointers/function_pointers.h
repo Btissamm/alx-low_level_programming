97a6cc0759c808786a6d70931dbd6759009d77d2
btissamm <btissam.mhanni1@gmail.com>
2023-05-24 13:05:43 -0700

    h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef funtion_pointers
-#define funtion_pointers
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 #include <stddef.h>
 #include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
