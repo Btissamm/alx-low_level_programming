aa175d4d600d19b9239583a8f86ea593e40b2d3a
btissamm <btissam.mhanni1@gmail.com>
2023-05-24 17:15:19 -0700

    r

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * factorial - returns the fact of a numb
+ * @n: the int
+ * Return: factorial
+ */
+int factorial(int n)
+{	if (n < 0)
+		return(-1);
+	if (n == 0)
+		return(1);
+	return (n * factorial(n - 1));
+}
