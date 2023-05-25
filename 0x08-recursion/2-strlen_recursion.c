0249feec5f51ca146ab4961f9fd513a650e9d320
btissamm <btissam.mhanni1@gmail.com>
2023-05-24 17:05:18 -0700

    r

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion -returns the length
+ * @s: the string
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		int _strlen_recursion(s+1);
+	}
+return (i);
+}
